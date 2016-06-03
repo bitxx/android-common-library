342f42b3a0f5d611d9144fc8a8c3e89110aabcac
wuj <idea_wj@163.com>
2016-06-03 20:10:19 +0800

    JSON原生生成与解析

--- app/src/main/java/com/wj/demo/ui/activity/JsonActivity.java
+++ app/src/main/java/com/wj/demo/ui/activity/JsonActivity.java
@@ -1,9 +1,7 @@
 package com.wj.demo.ui.activity;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;

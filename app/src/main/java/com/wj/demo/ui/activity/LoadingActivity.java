4547f572d42740ae15bd9419cb68bedfc58c52c1
wuj <idea_wj@163.com>
2016-06-06 17:32:17 +0800

    更新

--- app/src/main/java/com/wj/demo/ui/activity/LoadingActivity.java
+++ app/src/main/java/com/wj/demo/ui/activity/LoadingActivity.java
@@ -2,9 +2,6 @@ package com.wj.demo.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.wj.demo.R;
@@ -20,49 +17,24 @@ import com.wj.library.helper.UIHelper;
 public class LoadingActivity extends BaseActivity {
     private static String TAG = LoadingActivity.class.getName();
 
-    private final int CHECK_UPDATE = 1;
-
-    private Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what){
-                case CHECK_UPDATE:
-                    UIHelper.startActivityAndFinishin(LoadingActivity.this, new Intent(LoadingActivity.this, MainActivity.class));
-                    break;
-            }
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initView();
-        initData();
+
     }
 
     @Override
     protected void initView() {
+        super.initView();
         final View view = View.inflate(this, R.layout.activity_loading, null);
         setContentView(view);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(1800);
-                    Message msg = new Message();
-                    msg.what = CHECK_UPDATE;
-                    handler.sendMessage(msg);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+        UIHelper.startActivityAndFinishin(LoadingActivity.this, new Intent(LoadingActivity.this, MainActivity.class));
     }
 
     @Override
     protected void initData() {
+        super.initData();
         
     }
 }

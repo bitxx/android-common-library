60d524e35ece8f56365529ac0f2e7d7106cbac56
wuj <idea_wj@163.com>
2016-06-12 17:44:04 +0800

    * 功能：  * 1.为gif动画设置播放次数  * 2.设置动画是否暂停  * 3.设置动画恢复初始  * 4.监听指定次数播放完毕后的操作

--- app/src/main/java/com/wj/demo/ui/activity/DemoGifViewActivity.java
+++ app/src/main/java/com/wj/demo/ui/activity/DemoGifViewActivity.java
@@ -2,15 +2,25 @@ package com.wj.demo.ui.activity;
 
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.wj.demo.R;
 import com.wj.demo.ui.base.BaseActivity;
+import com.wj.library.helper.ToastHelper;
 import com.wj.library.helper.ToolbarHelper;
+import com.wj.library.widget.GifView;
 
 /**
  * Gif动画
  * Created by wuj on 2016/6/7.
+ * 使用须知：在该demo中，若设置播放次数，播放结束后，不应该再点击开始/暂停按钮，此逻辑在现实中不存在，
+ *
+ * 功能：
+ * 1.为gif动画设置播放次数
+ * 2.设置动画是否暂停
+ * 3.设置动画恢复初始
+ * 4.监听指定次数播放完毕后的操作
  * @version 1.0
  */
 public class DemoGifViewActivity extends BaseActivity {
@@ -18,14 +28,74 @@ public class DemoGifViewActivity extends BaseActivity {
 
     private Toolbar toolbar;
     private TextView tvTitle;
+    private GifView gif1;
+    private GifView gif2;
+    private GifView gif3;
+    private Button btChange;  //开始暂停
+    private Button btRestart;  //重置
 
     @Override
     protected void initView() {
         setContentView(R.layout.activity_demo_gif_view);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         tvTitle = (TextView)findViewById(R.id.tv_title);
+        gif1 = (GifView)findViewById(R.id.gif_1);
+        gif2 = (GifView)findViewById(R.id.gif_2);
+        gif3 = (GifView)findViewById(R.id.gif_3);
+        gif1.setTimes(1);
+        gif2.setTimes(2);
+        gif3.setTimes(5);
+        btChange = (Button)findViewById(R.id.bt_change);
+        btRestart = (Button)findViewById(R.id.bt_restart);
+        btChange.setOnClickListener(this);
+        btRestart.setOnClickListener(this);
         tvTitle.setText("GIF动画");
         ToolbarHelper.initToolbar(this,toolbar,R.mipmap.ic_back);
+
+        gif1.setOnFinishListener(new GifView.FinishListener() {
+            @Override
+            public void finish() {
+                ToastHelper.toastShort(DemoGifViewActivity.this,"第一个gif图片播放1次结束啦！");
+            }
+        });
+
+        gif2.setOnFinishListener(new GifView.FinishListener() {
+            @Override
+            public void finish() {
+                ToastHelper.toastShort(DemoGifViewActivity.this,"第二个gif图片播放2次结束啦！");
+            }
+        });
+
+        gif3.setOnFinishListener(new GifView.FinishListener() {
+            @Override
+            public void finish() {
+                ToastHelper.toastShort(DemoGifViewActivity.this,"第三个gif图片播放5次结束啦！");
+            }
+        });
     }
 
+    @Override
+    protected void myOnClick(View view) {
+        super.myOnClick(view);
+        switch (view.getId()){
+            case R.id.bt_change:
+                if(gif1.isPaused()){
+                    gif1.setPaused(false);
+                    gif2.setPaused(false);
+                    gif3.setPaused(false);
+                    btChange.setText("暂停");
+                }else {
+                    gif1.setPaused(true);
+                    gif2.setPaused(true);
+                    gif3.setPaused(true);
+                    btChange.setText("开始");
+                }
+                break;
+            case R.id.bt_restart:
+                gif1.restart();
+                gif2.restart();
+                gif3.restart();
+                break;
+        }
+    }
 }
--- library_wj/src/main/java/com/wj/library/widget/GifView.java
+++ library_wj/src/main/java/com/wj/library/widget/GifView.java
@@ -14,6 +14,13 @@ import android.widget.ImageView;
  * <p/>
  * Created by wuj on 2016/6/7.
  * 实现gif动画
+ * 已经关闭硬件加速，增强兼容性
+ *
+ * *功能：
+ * 1.为gif动画设置播放次数
+ * 2.设置动画是否暂停
+ * 3.设置动画恢复初始
+ * 4.监听指定次数播放完毕后的操作
  */
 public class GifView extends ImageView {
     private static final String TAG = GifView.class.getSimpleName();
@@ -23,6 +30,12 @@ public class GifView extends ImageView {
     private Movie movie = null;  //用于存储帧
 
     private int srcId = -1;   //用于保存重组件src中获取到的图片资源
+
+    private volatile boolean isPaused = false;  //是否暂停，默认否
+    private int duration;   //动画总时长
+    private int nowRealTime;  //当前播放的帧的时间
+    private int times = 0;  //gif动画播放次数，0则代表无限制播放
+    private FinishListener finishListener;   //监听播放结束，只有播放次数times>0时候有效
 
     public GifView(Context context) {
         super(context);
@@ -51,8 +64,13 @@ public class GifView extends ImageView {
         }
 
         srcId = attrs.getAttributeResourceValue("http://schemas.android.com/apk/res/android", "src", 0);
-        if (srcId > 0)
+        if (srcId > 0) {
             movie = Movie.decodeStream(getResources().openRawResource(srcId));
+            if (movie != null)
+                duration = movie.duration();//获取gif持续时间
+            //Log.e(TAG,duration+"");
+        }
+
     }
 
     @Override
@@ -61,22 +79,41 @@ public class GifView extends ImageView {
         drawGif(canvas);
     }
 
-    public void drawGif(Canvas canvas) {
-        nowTime = android.os.SystemClock.uptimeMillis();  //开机到现在的毫秒数，不能使用System.currentTimeMillis，耗时太长，动画会卡
-        if (perTime == 0) {  //说明动画刚刚开始
-            perTime = nowTime;
-        }
-        if (movie != null) {
-            int duration = movie.duration();//获取gif持续时间
-            int nowRealTime = (int) ((nowTime - perTime) % duration); //准确计算出当前点时间
-            movie.setTime(nowRealTime);
-            canvas.save(Canvas.MATRIX_SAVE_FLAG);
-            canvas.restore();
-            movie.draw(canvas, 0, 0);
-            invalidateView(); //清空当前画面
-        }
+    private void drawGif(Canvas canvas) {
+        if (!isPaused) {
+            nowTime = android.os.SystemClock.uptimeMillis();  //开机到现在的毫秒数，不能使用System.currentTimeMillis，耗时太长，动画会卡
+            if (perTime == 0)  //说明动画刚刚开始
+                perTime = nowTime;
+            long temp = nowTime - perTime;
+
+            if(((temp/duration)<times&&times>0)||times<=0) {  //
+                nowRealTime = (int) (temp % duration); //准确计算出当前点时间
+
+                if (movie != null) {
+                    drawFrame(canvas);
+                    invalidateView(); //清空当前画面
+                }
+            }else
+                finishListener.finish();
+        } else
+            drawFrame(canvas);
     }
 
+    /**
+     * 画出当前帧
+     *
+     * @param canvas
+     */
+    private void drawFrame(Canvas canvas) {
+        movie.setTime(nowRealTime);
+        canvas.save(Canvas.MATRIX_SAVE_FLAG);
+        canvas.restore();
+        movie.draw(canvas, 0, 0);
+    }
+
+    /**
+     * 重置
+     */
     @SuppressLint("NewApi")
     private void invalidateView() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
@@ -84,5 +121,52 @@ public class GifView extends ImageView {
         } else {
             invalidate();
         }
+    }
+
+    /**
+     * 判断是否暂停状态
+     *
+     * @return
+     */
+    public boolean isPaused() {
+        return isPaused;
+    }
+
+    /**
+     * 设置暂停
+     *
+     * @param paused
+     */
+    public void setPaused(boolean paused) {
+        isPaused = paused;
+        if (!paused) {
+            perTime = android.os.SystemClock.uptimeMillis() - nowRealTime; //当前时间距离上次暂停帧时候
+        }
+        invalidateView();
+    }
+
+    /**
+     * 重置动画
+     */
+    public void restart() {
+        perTime = 0;
+        invalidateView();
+    }
+
+    /**
+     * 只有设置的播放次数times>0时候有效
+     * @param finishListener
+     */
+    public void setOnFinishListener(FinishListener finishListener){
+        this.finishListener = finishListener;
+    }
+
+    public void setTimes(int times) {
+        this.times = times;
+    }
+
+
+    public interface FinishListener{
+        void finish();
     }
 }

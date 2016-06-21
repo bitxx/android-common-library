db22bd5333511ae4c6d5b5b50052110a4924a8fa
wuj <idea_wj@163.com>
2016-06-21 14:23:14 +0800

    自定义展示类型

--- library_wj/src/main/java/com/wj/library/widget/MagicalImageView.java
+++ library_wj/src/main/java/com/wj/library/widget/MagicalImageView.java
@@ -1,6 +1,7 @@
 package com.wj.library.widget;
 
 import android.content.Context;
+import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
@@ -15,6 +16,9 @@ import android.widget.ImageView;
  *
  */
 public class MagicalImageView extends ImageView implements ViewTreeObserver.OnGlobalLayoutListener {
+
+    private Type type = Type.FIT_CENTER;
+
     public MagicalImageView(Context context) {
         super(context);
     }
@@ -27,12 +31,43 @@ public class MagicalImageView extends ImageView implements ViewTreeObserver.OnGl
         super(context, attrs, defStyleAttr);
     }
 
-    public void setType(){}
+    /**
+     * 设置图片类型
+     * @param type
+     */
+    public void setType(Type type){
+        this.type = type;
+    }
 
 
     @Override
     public void onGlobalLayout() {
 
+    }
+
+    /**
+     * 图片初始化时候，显示的模式，
+     * 由于重写了ImageView缘故，原先的ScaleType不能使用，需要在此使用setType()方法
+     */
+    public enum Type{
+        IT_XY      (1),
+
+        FIT_START   (2),
+
+        FIT_CENTER  (3),
+
+        FIT_END     (4),
+
+        CENTER      (5),
+
+        CENTER_CROP (6),
+
+        CENTER_INSIDE (7);
+
+        Type(int type) {
+            this.type = type;
+        }
+        final int type;
     }
 
 }

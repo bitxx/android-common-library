3b025e9bb0e6a666fce87bcb174860d611a7cff3
idea_wj <idea_wj@163.com>
2016-06-26 01:40:26 +0800

    ClearEditText优化

--- library_wj/src/main/java/com/wj/library/widget/ClearEditText.java
+++ library_wj/src/main/java/com/wj/library/widget/ClearEditText.java
@@ -113,27 +113,4 @@ public class ClearEditText extends AutoCompleteTextView implements View.OnFocusC
     public void afterTextChanged(Editable s) {
 
     }
-
-    /**
-     * 设置晃动动画
-     */
-    public void setShakeAnimation() {
-        this.setAnimation(shakeAnimation(5));
-    }
-
-    /**
-     * 晃动动画
-     *
-     * @param counts
-     *			1秒钟晃动多少下
-     * @return
-     */
-    public static Animation shakeAnimation(int counts) {
-        Animation translateAnimation = new TranslateAnimation(0, 10, 0, 0);
-        //设置一个循环加速器，使用传入的次数就会出现摆动的效果。
-        translateAnimation.setInterpolator(new CycleInterpolator(counts));
-        translateAnimation.setDuration(500);
-        return translateAnimation;
-    }
-
 }

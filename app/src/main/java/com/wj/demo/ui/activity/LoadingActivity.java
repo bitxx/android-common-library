112d226ec80b3f9c71d397b2f851471914cfffe5
idea_wj <idea_wj@163.com>
2016-06-26 01:29:22 +0800

    启动界面替换为GIF动画

--- app/src/main/java/com/wj/demo/ui/activity/LoadingActivity.java
+++ app/src/main/java/com/wj/demo/ui/activity/LoadingActivity.java
@@ -1,12 +1,11 @@
 package com.wj.demo.ui.activity;
 
 import android.content.Intent;
-import android.os.Handler;
-import android.os.Message;
 
 import com.wj.demo.R;
 import com.wj.demo.ui.base.BaseActivity;
 import com.wj.library.helper.UIHelper;
+import com.wj.library.widget.GifView;
 
 /**
  * @version 1.0
@@ -15,36 +14,18 @@ import com.wj.library.helper.UIHelper;
  */
 public class LoadingActivity extends BaseActivity {
     private static String TAG = LoadingActivity.class.getName();
-
-    private Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what){
-                case 1:
-                    UIHelper.startActivityAndFinishin(LoadingActivity.this, new Intent(LoadingActivity.this, MainActivity.class));
-                    break;
-            }
-        }
-    };
+    private GifView gifLodaing;
 
     @Override
     protected void initView() {
         setContentView(R.layout.activity_loading);
-
-        //暂时休眠1800过渡，后面再处理
-        new Thread(new Runnable() {
+        gifLodaing = (GifView) findViewById(R.id.gif_loading);
+        gifLodaing.setTimes(1);
+        gifLodaing.setOnFinishListener(new GifView.FinishListener() {
             @Override
-            public void run() {
-                try {
-                    Thread.sleep(1800);
-                    Message msg = new Message();
-                    msg.what = 1;
-                    handler.sendMessage(msg);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            public void finish() {
+                UIHelper.startActivityAndFinishin(LoadingActivity.this, new Intent(LoadingActivity.this, MainActivity.class));
             }
-        }).start();
+        });
     }
 }

a9b1fc553e7170f53d3378c9c3679ec1bb67ae95
idea_wj <idea_wj@163.com>
2016-06-26 00:02:51 +0800

    优化代码

--- library_wj/src/main/java/com/wj/library/widget/CustomerScrollView.java
+++ library_wj/src/main/java/com/wj/library/widget/CustomerScrollView.java
@@ -12,6 +12,10 @@ import android.widget.ScrollView;
 
 /**
  * @version 1.0
+ *
+ * 注释都比较详细,如有不清楚地方,可以联系作者:
+ * https://github.com/jason-wj/android-common-library
+ *
  * 在xml中加入此scroll后，其中的view上下滑动有弹性特效
  * 借用了开源中国OSC客户端源码
  *
@@ -40,9 +44,8 @@ public class CustomerScrollView extends ScrollView {
 	@Override
 	protected void onFinishInflate() {
 		super.onFinishInflate();
-		if (getChildCount() > 0) {
+		if (getChildCount() > 0)
 			inner = getChildAt(0); //相当于获取scrollview中，子控件的第一个，控制该子控件的位置
-		}
 	}
 
 	@SuppressLint("ClickableViewAccessibility")
@@ -61,37 +64,30 @@ public class CustomerScrollView extends ScrollView {
 		switch (action) {
 		case MotionEvent.ACTION_DOWN:
 			y = ev.getY();
-			Log.e(TAG,"action_down,y="+y);
 			break;
+
 		case MotionEvent.ACTION_UP:
-			Log.e(TAG,"action_up");
-			if (isNeedAnimation()) {
-				// Log.v("mlguitar", "will up and animation");
+			if (isNeedAnimation())
 				animation();
-			}
 			break;
+
 		case MotionEvent.ACTION_MOVE: //滑动期间，该事件会一直被触发
 			final float preY = y;
-			Log.e(TAG,"preY:"+preY);
 			float nowY = ev.getY();
-			//Log.e(TAG,"action_move,preY="+preY+" nowY:"+nowY);
+
 			/**
 			 * size=4 表示 拖动的距离为屏幕的高度的1/4
 			 */
 			int deltaY = (int) (preY - nowY) / size;  //每两次滑动间隔的差距都除以4，这样最终下滑距离也只有屏幕1/4
-			// 滚动
-			// scrollBy(0, deltaY);
 
 			y = nowY; //每次下滑都重新更新起始坐标
 			if (isNeedMove()) {
 				if (normal.isEmpty()) {  //normal初始化与第一个子组件的大小一样
 					normal.set(inner.getLeft(), inner.getTop(),
 							inner.getRight(), inner.getBottom());
-					//Log.e(TAG,"action_move,inner.getLeft()="+inner.getLeft()+" inner.getTop():"+inner.getTop()+" inner.getRight():"+inner.getRight()+" inner.getBottom()):"+inner.getBottom()+" all");
 					return;
 				}
 				int yy = inner.getTop() - deltaY;  //deltaY下拉时候本身是负数，类似匀速增加y坐标，inner.getTop()坐标一直在累加
-				//Log.e(TAG,"inner.getTop():"+inner.getTop()+",yy:"+yy+",preY:"+preY+",nowY:"+nowY);
 
 				// 移动布局
 				inner.layout(inner.getLeft(), yy, inner.getRight(),

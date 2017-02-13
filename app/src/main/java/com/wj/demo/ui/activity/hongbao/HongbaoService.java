e1c74b2ed4e80d1425e63067cd6b9de935393633
idea_wj <idea_wj@163.com>
2017-02-13 22:56:44 +0800

    稳定性修复

--- app/src/main/java/com/wj/demo/ui/activity/hongbao/HongbaoService.java
+++ app/src/main/java/com/wj/demo/ui/activity/hongbao/HongbaoService.java
@@ -1,20 +1,15 @@
 package com.wj.demo.ui.activity.hongbao;
-
 import android.accessibilityservice.AccessibilityService;
-import android.app.Service;
 import android.content.Intent;
-import android.support.annotation.IntDef;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 import com.wj.library.util.LogUtil;
 
 import java.util.List;
-
 /**
  * Created by idea_wj on 2017/2/13.
  */
-
 public class HongbaoService extends AccessibilityService {
 
     private String[] filter = new String[]{"查看红包","领取红包"};
@@ -23,16 +18,14 @@ public class HongbaoService extends AccessibilityService {
     public int onStartCommand(Intent intent, int flags, int startId) {
         LogUtil.e("HELLO","HELLO");
         return super.onStartCommand(intent, flags, startId);
-
     }
 
     //监听窗口变化的回调
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         AccessibilityNodeInfo info = event.getSource(); //树形结构
-        if(info == null){
+        if(info == null)
             return;
-        }
         startClick(info);
     }
 
@@ -41,19 +34,16 @@ public class HongbaoService extends AccessibilityService {
         if(list == null)
             return;
         AccessibilityNodeInfo nodeInfo = list.get(list.size()-1);  //最后一个红包
-        if("已拆开".equals(nodeInfo.getText())){
+        if("已拆开".equals(nodeInfo.getText()))
             return;
-        }
         boolean isChceked = nodeInfo.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK); //模拟点击,由父类点击
-        if(!isChceked){
+        if(!isChceked)
             reClick(info);
-        }
     }
 
     @Override
     protected void onServiceConnected() {
-        super.onServiceConnected();
-        //可以用来配置accessibilityservice具体要监听的内容，也可以通过config.xml来进行配置
+        super.onServiceConnected();//可以用来配置accessibilityservice具体要监听的内容，也可以通过config.xml来进行配置
     }
 
     //拿子view实现点击
@@ -66,11 +56,10 @@ public class HongbaoService extends AccessibilityService {
                 AccessibilityNodeInfo nodeInfo = info.getChild(i);
                 if(nodeInfo == null)
                     continue;
-                if (nodeInfo.getChildCount() > 0) {
+                if (nodeInfo.getChildCount() > 0)
                     reClick(nodeInfo);
-                }else {
+                else
                     info.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                }
             }
         }
     }
@@ -80,16 +69,12 @@ public class HongbaoService extends AccessibilityService {
            List<AccessibilityNodeInfo> list =  info.findAccessibilityNodeInfosByText(text);
             if(list != null&&!list.isEmpty()){
                 return list;
-
             }
         }
         return null;
-
     }
 
     //中断服务的回调
     @Override
-    public void onInterrupt() {
-
-    }
+    public void onInterrupt() {}
 }
